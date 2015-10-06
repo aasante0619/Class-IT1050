e085db050ef5a3c9ad3d0da29fbf29deeade0d1a
aasante0619 <abolden817@hotmail.com>
2015-10-06 18:45:03 -0400

    Redid Lab 2
    Started Over again on Lab 2

---
+++ Lab 2 Redo/Lab 2 Redo/Person.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2_Redo
+{
+    class Person
+    {
+
+        public string FirstName;
+        public string LastName;
+        public int Age;
+        public Person Spouse;
+
+        public static double AverageAge()
+        {
+            return (double)Person.SumOfAges / (double)Person.Count;
+
+        }
+
+
+        public static int SumOfAges;
+
+        public static int Count;
+
+        public string GetFullName()
+        {
+            System.Console.WriteLine("What is the person's first name?");
+            this.FirstName = System.Console.ReadLine();
+            System.Console.WriteLine("What is the person's last name?");
+            this.LastName = System.Console.ReadLine();
+
+            return (this.FirstName + this.LastName);
+
+            
+        }
+
+
+        public void  GetAge()
+        {
+
+            System.Console.WriteLine("How old is the person?");
+            this.Age = int.Parse(System.Console.ReadLine());
+
+
+        }
+  
+
+        public void PrintNameAndAge()
+        {
+
+            System.Console.WriteLine(this.FirstName + " " + this.LastName + " " + "is" + " " + this.Age + " " + "years old");
+
+        }
+
+
+        public void CreateSpouseIfMarried()
+        {
+            System.Console.WriteLine("Are you married? Answer True or False");
+            if (System.Console.ReadLine().ToLower().StartsWith("t"))
+
+
+            {
+                this.Spouse = new Person ();
+                this.Spouse.GetFullName();
+                this.Spouse.GetAge();
+                this.Spouse.PrintNameAndAge();
+                this.Spouse.Spouse = this;
+
+
+
+
+
+
+            }
+
+
+        }
+
+    }
+}
---
+++ Lab 2 Redo/Lab 2 Redo/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2_Redo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person.Count = 0;
+            Person.SumOfAges = 0;
+
+
+            Person person1 = new Person();
+            person1.GetFullName();
+            person1.GetAge();
+            person1.PrintNameAndAge();
+            person1.CreateSpouseIfMarried();
+     
+     
+            
+           
+
+
+            Person.Count ++;
+            Person.SumOfAges += person1.Age;
+
+
+
+            Person person2 = new Person();
+            person2.GetFullName();
+            person2.GetAge();
+            person2.PrintNameAndAge();
+            person2.CreateSpouseIfMarried();
+
+
+            Person.Count++;
+            Person.SumOfAges += person2.Age;
+
+            Person person3 = new Person();
+            person3.GetFullName();
+            person3.GetAge();
+            person3.PrintNameAndAge();
+            person3.CreateSpouseIfMarried();
+
+
+            Person.Count++;
+            Person.SumOfAges += person3.Age;
+
+            System.Console.WriteLine("Average Age: " + Person.AverageAge());
+            System.Console.ReadKey();
+
+        }
+    }
+}
