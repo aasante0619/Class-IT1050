981d5a41233b9c740fb63cbe3ff87263a4637723
aasante0619 <abolden817@hotmail.com>
2015-10-20 19:03:12 -0400

    started lab 3

---
+++ Lab3/Lab3/Instructor.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    class Instructor
+    {
+
+        private string FirstName;
+        private string LastName;
+        private string CourseName;
+        private string fullName;
+
+        public Instructor (string instructoFirstName, string instructorLastName, string CourseNameGiven)
+        {
+            this.fullName = instructoFirstName + " " + instructorLastName;
+            this.CourseName = CourseNameGiven;
+
+
+        }
+
+        public void SetStudentGrade(Student studentName, int GradeGiven)
+        {
+
+            GradeGiven = studentName.Grade;
+
+
+       
+
+        }
+
+        public void PrintInfo(Instructor LastName, Instructor CourseName)
+        {
+
+            System.Console.WriteLine("Instructor " + this.fullName + " " + "teaches" + " " + this.CourseName);
+            
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+    
+
+
+   
+
+
+}
+
+
+
+
+
+
---
+++ Lab3/Lab3/Student.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace Lab3
+{
+    class Student
+    {
+        private string FirstName;
+        private string LastName;
+        public string Grade;
+        private Instructor Teacher; 
+        
+
+        public Student (string studentFirstName, string studentLastName, Instructor LastName)
+
+        {
+            this.FirstName = studentFirstName;
+            this.LastName = studentLastName;
+            this.Teacher = LastName;
+            
+        }
+        
+
+
+
+
+
+
+
+
+    }
+}
