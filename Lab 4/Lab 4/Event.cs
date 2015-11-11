af2909271b71d3c9a1c5960443f28820dc084009
aasante0619 <abolden817@hotmail.com>
2015-11-10 19:01:44 -0500

    Added Lab 4

---
+++ Lab 4/Lab 4/Event.cs
@@ -0,0 +1,99 @@
+﻿
+
+namespace Lab_4
+{
+    class Event
+    {
+        public string movieGoer;
+        public int Age;
+        public double timeOfDay;
+        public int numberOfLargeSoda;
+        public int numberOfSmallSoda;
+        public int numberOfHotDogs;
+        public int numberOfPopcorn;
+        public int numberOfCandies;
+
+
+
+        public double TicketPrice;
+
+        
+        public void GetEventInfo()
+        {
+            System.Console.WriteLine(" What is the name of the moviegoer?");
+            this.movieGoer = System.Console.ReadLine();
+
+            System.Console.WriteLine("What is the moviegoer's age?");
+            this.Age = int.Parse(System.Console.ReadLine());
+
+            System.Console.WriteLine("What is the time of day (in military time)?");
+            this.timeOfDay = int.Parse(System.Console.ReadLine());
+
+            System.Console.WriteLine("How many large sodas does the moviegoer want?");
+            this.numberOfLargeSoda = int.Parse(System.Console.ReadLine());
+
+            System.Console.WriteLine("How many small sodas does the moviegoer want?");
+            this.numberOfSmallSoda = int.Parse(System.Console.ReadLine());
+
+            System.Console.WriteLine("How many hot dogs does the moviegoer want?");
+            this.numberOfHotDogs = int.Parse(System.Console.ReadLine());
+
+            System.Console.WriteLine("How many orders of popcorn does the moviegoer want?");
+            this.numberOfPopcorn = int.Parse(System.Console.ReadLine());
+
+            System.Console.WriteLine("How many orders of Candies does the moviegoer want?");
+            this.numberOfCandies = int.Parse(System.Console.ReadLine());
+
+           
+        }
+
+
+        
+
+        public void CalculateTicketPrice()
+        {
+            if( this.Age < 17 && this.timeOfDay < 1200)
+            {
+                TicketPrice = 3.99;
+            }
+                
+            if (this.Age < 17 && this.timeOfDay > 1200)
+
+            {
+                TicketPrice = 6.99;
+            }
+
+            if (this.Age > 17 && this.Age < 55 && this.timeOfDay < 1200)
+
+            {
+                TicketPrice = 5.99;
+            }
+
+            if (this.Age > 17 && this.Age < 55 && this.timeOfDay > 1200)
+
+            {
+                TicketPrice = 10.99;
+            }
+
+            if (this.Age > 17 && this.Age > 55 && this.timeOfDay < 1200)
+
+            {
+                TicketPrice = 4.50;
+            }
+
+
+            if (this.Age > 17 && this.Age > 55 && this.timeOfDay > 1200)
+
+            {
+                TicketPrice = 8.50;
+            }
+        }
+
+      
+        
+
+
+
+        }
+    }
+
---
+++ Lab 4/Lab 4/Program.cs
@@ -0,0 +1,50 @@
+﻿
+
+namespace Lab_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Event Event1 = new Event();
+
+            Event1.GetEventInfo();
+            Event1.CalculateTicketPrice();
+            System.Console.WriteLine("Your total ticket cost is  " + Event1.TicketPrice);
+
+            System.Console.WriteLine("---------------------------------------------------");
+
+
+
+
+            Event Event2 = new Event();
+
+            Event2.GetEventInfo();
+            Event2.CalculateTicketPrice();
+            System.Console.WriteLine("Your total ticket cost is  " + Event2.TicketPrice);
+
+            System.Console.WriteLine("---------------------------------------------------");
+
+
+
+            Event Event3 = new Event();
+
+            Event3.GetEventInfo();
+            Event3.CalculateTicketPrice();
+            System.Console.WriteLine("Your total ticket cost is  " + Event3.TicketPrice);
+
+            System.Console.WriteLine("---------------------------------------------------");
+
+
+
+            Event Event4 = new Event();
+
+            Event4.GetEventInfo();
+            Event4.CalculateTicketPrice();
+            System.Console.WriteLine("Your total ticket cost is  " + Event4.TicketPrice);
+
+
+
+        }
+    }
+}
