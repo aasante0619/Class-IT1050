1ac28d9b6b08fdd7c8224b3cdaa77e272ff5bff5
aasante0619 <abolden817@hotmail.com>
2015-11-17 19:05:37 -0500

    still trying to merge files
    merging files

---
+++ Lab 4 redo/Event.cs
@@ -0,0 +1,142 @@
+﻿
+
+namespace Lab_4
+{
+    class Event
+    {
+        public string movieGoer;
+        public int movieGoerAge;
+        public double timeOfDay;
+        public int numberOfLargeSoda;
+        public int numberOfSmallSoda;
+        public int numberOfHotDogs;
+        public int numberOfPopcorn;
+        public int numberOfCandies;
+        public double concessionPrice;
+        public double DiscountedConcessionPrice;
+        public double TotalPrice;
+
+
+
+
+        public double FoodPriceTotal;
+
+
+        public double TicketPrice;
+        public double ConcessionPrice;
+
+ 
+
+        public void calculateTicketPrice()
+        {
+            if (this.timeOfDay < 1200 && this.movieGoerAge < 18)
+            {
+                TicketPrice = 3.99;
+
+            }
+            if (this.timeOfDay < 1200 && this.movieGoerAge > 18 && this.movieGoerAge < 65)
+            {
+                TicketPrice = 5.99;
+            }
+
+                      
+            if (this.timeOfDay < 1200 && this.movieGoerAge > 18 && this.movieGoerAge > 65)
+            {
+
+                TicketPrice = 4.50;
+            }
+
+
+            if (this.timeOfDay > 1200 && this.movieGoerAge < 18)
+            {
+                TicketPrice = 6.99;
+
+            }
+            if (this.timeOfDay > 1200 && this.movieGoerAge > 18 && this.movieGoerAge < 65)
+            {
+                TicketPrice = 10.99;
+            }
+
+
+            if (this.timeOfDay < 1200 && this.movieGoerAge > 18 && this.movieGoerAge > 65)
+            {
+
+                TicketPrice = 8.50;
+            }
+
+
+        }
+
+        public void GetEventInfo()
+        {
+
+            System.Console.WriteLine(" What is the name of the moviegoer?");
+            this.movieGoer = System.Console.ReadLine();
+
+            System.Console.WriteLine("What is the moviegoer's age?");
+            this.movieGoerAge = int.Parse(System.Console.ReadLine());
+
+            System.Console.WriteLine("What is the time of day (in military time)?");
+            this.timeOfDay = int.Parse(System.Console.ReadLine());
+
+            System.Console.WriteLine("How many large sodas does the moviegoer want?");
+            this.numberOfLargeSoda = int.Parse(System.Console.ReadLine());
+
+            System.Console.WriteLine("How many small sodas does the moviegoer want?");
+            this.numberOfSmallSoda = int.Parse(System.Console.ReadLine());
+
+            System.Console.WriteLine("How many hot dogs does the moviegoer want?");
+            this.numberOfHotDogs = int.Parse(System.Console.ReadLine());
+
+            System.Console.WriteLine("How many orders of popcorn does the moviegoer want?");
+            this.numberOfPopcorn = int.Parse(System.Console.ReadLine());
+
+            System.Console.WriteLine("How many orders of Candies does the moviegoer want?");
+            this.numberOfCandies = int.Parse(System.Console.ReadLine());
+
+        }
+
+        public void  GetConcessionPrice()
+        {
+
+            concessionPrice = 0;
+            concessionPrice += (this.numberOfLargeSoda * 5.99);
+            concessionPrice += (this.numberOfSmallSoda * 3.50);
+            concessionPrice += (this.numberOfHotDogs * 3.99);
+            concessionPrice += (this.numberOfPopcorn* 4.50);
+            concessionPrice += (this.numberOfCandies * 1.99);
+            
+        }
+
+        public void GetDiscountedConcessionPrice()
+        {
+
+            DiscountedConcessionPrice = this.concessionPrice;
+
+            if (this.numberOfPopcorn > 0 && this.numberOfLargeSoda > 0)
+                {
+                this.TicketPrice -= 2;
+                }
+            
+            if (this.timeOfDay > 1200)
+            {
+                DiscountedConcessionPrice = this.concessionPrice - 1.48;
+            }
+
+            this.DiscountedConcessionPrice -= (1.99  *(numberOfCandies /4));
+
+         }
+
+        public void GetTotalPrice()
+    
+        {
+            this.TotalPrice = 0;
+            this.TotalPrice += this.DiscountedConcessionPrice;
+            this.TotalPrice += this.TicketPrice;
+
+
+    
+
+    }
+}
+
---
+++ Lab 4 redo/Program.cs
@@ -0,0 +1,97 @@
+﻿
+
+namespace Lab_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Event Event1 = new Event();
+
+
+            Event1.GetEventInfo();
+            Event1.calculateTicketPrice();
+            Event1.GetConcessionPrice();
+            Event1.GetDiscountedConcessionPrice();
+            Event1.GetTotalPrice();
+
+            System.Console.WriteLine("Your total ticket cost is  " + Event1.TicketPrice);
+            System.Console.WriteLine("Your total food cost is  " + Event1.FoodPriceTotal);
+
+
+            System.Console.WriteLine("Your total ticket cost is  " + Event1.TicketPrice);
+            System.Console.WriteLine("Your total concession cost is  " + Event1.concessionPrice);
+            System.Console.WriteLine("Your total concession cost with the discount is  " + Event1.DiscountedConcessionPrice);
+            System.Console.WriteLine("---------------------------------------------------");
+
+
+
+
+            Event Event2 = new Event();
+
+
+            Event2.GetEventInfo();
+            Event2.calculateTicketPrice();
+            Event2.GetConcessionPrice();
+            Event2.GetDiscountedConcessionPrice();
+            Event2.GetTotalPrice();
+            System.Console.WriteLine("Your total ticket cost is  " + Event2.TicketPrice);
+            System.Console.WriteLine("Your total concession cost is  " + Event2.concessionPrice);
+            System.Console.WriteLine("Your total concession cost with the discount is  " + Event2.DiscountedConcessionPrice);
+
+            System.Console.WriteLine("Your total ticket cost is  " + Event2.TicketPrice);
+            System.Console.WriteLine("Your total food cost is  " + Event2.FoodPriceTotal);
+
+
+            System.Console.WriteLine("---------------------------------------------------");
+
+
+
+            Event Event3 = new Event();
+
+            Event3.GetEventInfo();
+            Event3.calculateTicketPrice();
+            Event3.GetConcessionPrice();
+            Event3.GetDiscountedConcessionPrice();
+            Event3.GetTotalPrice();
+            System.Console.WriteLine("Your total ticket cost is  " + Event3.TicketPrice);
+            System.Console.WriteLine("Your total concession cost is  " + Event3.concessionPrice);
+            System.Console.WriteLine("Your total concession cost with the discount is  " + Event3.DiscountedConcessionPrice);
+
+            System.Console.WriteLine("Your total ticket cost is  " + Event3.TicketPrice);
+            System.Console.WriteLine("Your total food cost is  " + Event3.FoodPriceTotal);
+
+            System.Console.WriteLine("---------------------------------------------------");
+
+
+
+            Event Event4 = new Event();
+
+
+            Event4.GetEventInfo();
+            Event4.calculateTicketPrice();
+            Event4.GetConcessionPrice();
+            Event4.GetDiscountedConcessionPrice();
+            Event4.GetTotalPrice();
+            System.Console.WriteLine("Your total ticket cost is  " + Event4.TicketPrice);
+            System.Console.WriteLine("Your total concession cost is  " + Event4.concessionPrice);
+            System.Console.WriteLine("Your total concession cost with the discount is  " + Event4.DiscountedConcessionPrice);
+            System.Console.WriteLine("---------------------------------------------------");
+
+
+            System.Console.WriteLine("Total Event Cost for " + Event1.movieGoer + " is  " + Event1.TotalPrice + " .");
+            System.Console.WriteLine("Total Event Cost for " + Event2.movieGoer + " is  " + Event2.TotalPrice + " .");
+            System.Console.WriteLine("Total Event Cost for " + Event3.movieGoer + " is  " + Event3.TotalPrice + " .");
+            System.Console.WriteLine("Total Event Cost for " + Event4.movieGoer + " is  " + Event4.TotalPrice + " .");
+
+            System.Console.WriteLine("Grand Total for ALL MOVIEGOERS IS  " + (Event1.TotalPrice + Event2.TotalPrice+ Event3.TotalPrice+ Event4.TotalPrice) + "  wow!");
+
+            System.Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
