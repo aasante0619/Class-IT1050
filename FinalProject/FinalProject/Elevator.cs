c0f7b271df95bf572fa39c3f13a4cc8318378b04
aasante0619 <abolden817@hotmail.com>
2015-12-06 21:33:29 -0500

    Began Final Project

---
+++ FinalProject/FinalProject/Elevator.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject
+{
+    class Elevator
+    {
+
+        private double MaxWeight;
+        public Passenger[] Occupants;
+
+        public Elevator (int maxOccupants, double maxWeight)
+
+        {
+
+            Occupants = new Passenger[maxOccupants];
+
+            MaxWeight = maxWeight;
+            
+        }
+
+
+
+
+
+        public void AddOccupant(int index, Passenger passenger)
+
+        {
+
+            Occupants[index] = passenger;
+           
+            
+        }
+
+
+        public double GetCurrentWeight()
+
+
+        {
+
+            double elevatorweight = 0;
+
+           elevatorweight += Passenger.GetWeight()
+
+        }
+
+        public bool isOverCapacity()
+
+        {
+
+        if (GetCurrentWeight() > MaxWeight)
+
+            {
+
+                return true;
+            }
+
+        else
+
+            {
+
+                return false;
+            }
+
+
+
+        }
+    }
+
+}
---
+++ FinalProject/FinalProject/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Elevator elevator1 = new Elevator(2, 400);
+
+            Passenger A1 = new Passenger("A1", 180);
+            elevator1.AddOccupant(0, A1);
+
+            Passenger A2 = new Passenger("A2", 220);
+            elevator1.AddOccupant(1, A2);
+
+            elevator1.isOverCapacity();
+
+
+            Elevator elevator2 = new Elevator(3, 600);
+
+            Passenger A1 = new Passenger("A1", 200);
+            elevator1.AddOccupant(0, A1);
+
+            Passenger A2 = new Passenger("A2", 200);
+            elevator1.AddOccupant(1, A1);
+
+            Passenger A3 = new Passenger("A3", 201);
+            elevator1.AddOccupant(2, A1);
+
+            elevator2.isOverCapacity();
+
+        }
+    }
+}
